88cbade79f20ce8291fce15c7675c7c2918bf144
Egor <goro4@mail.ru>
2023-07-07 17:27:16 +0500

    all work

---
+++ backend-chat/src/main/java/com/kafka/demo/consumer/MessageListener.java
@@ -0,0 +1,26 @@
+package com.kafka.demo.consumer;
+
+import com.kafka.demo.model.Message;
+import com.kafka.demo.utils.KafkaConstants;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListener {
+    final
+    SimpMessagingTemplate template;
+
+    public MessageListener(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @KafkaListener(
+            topics = KafkaConstants.KAFKA_TOPIC,
+            groupId = KafkaConstants.GROUP_ID
+    )
+    public void listen(Message message) {
+        System.out.println("Отправка сообщений всем пользователям");
+        template.convertAndSend("/topic/group", message);
+    }
+}
