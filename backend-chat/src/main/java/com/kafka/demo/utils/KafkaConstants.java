367c6959d18cde2449f982272357470219f943d9
Egor <goro4@mail.ru>
2023-07-10 17:25:13 +0500

    add docker-compose for kafka

--- backend-chat/src/main/java/com/kafka/demo/utils/KafkaConstants.java
+++ backend-chat/src/main/java/com/kafka/demo/utils/KafkaConstants.java
@@ -4,5 +4,5 @@ public class KafkaConstants {
     public static final String KAFKA_TOPIC = "kafka-chat";
     public static final String GROUP_ID = "kafka-sandbox";
 
-    public static final String KAFKA_BROKER = "localhost:9092";
+    public static final String KAFKA_BROKER = "localhost:29092";//9092
 }
